426fb2fc75eef58bfa66cbed1831f516b1cb4728
spree@bluebash.co <developer@bluebash.co>
2022-03-29 13:10:39 +0530

    added controller and routes for the order notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Spree::Core::Engine.add_routes do
-  # Add your extension routes here
+  namespace :admin do
+    resources :orders, only: [] do
+      namespace :orders do
+        resources :notes, only: %i[index create]
+      end
+    end
+  end
 end
