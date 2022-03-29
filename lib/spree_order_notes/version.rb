46ef816f83879c7c0f4701d64a4939e99a03cec6
spree@bluebash.co <developer@bluebash.co>
2022-03-29 12:45:59 +0530

    init app

---
+++ lib/spree_order_notes/version.rb
@@ -0,0 +1,11 @@
+module SpreeOrderNotes
+  VERSION = '0.0.1'.freeze
+
+  module_function
+
+  # Returns the version of the currently loaded SpreeOrderNotes as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION
+  end
+end
